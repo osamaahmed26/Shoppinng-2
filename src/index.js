3013981c6c7eb16c3ecf8186c6ef1a38b57cf70f
osamaahmed26 <your-email@example.com>
2025-11-14 17:50:14 +0200

    Shoppinng-Land

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <CartProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CartProvider>
+  <CartProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </CartProvider>
 );
